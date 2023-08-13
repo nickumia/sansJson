32076f77eb9cc8991527eaf86b3e3d225695073c
nickumia <nickumia6@gmail.com>
2023-08-13 10:06:14 -0400

    new: make nonhomogenous test more comprehensive

--- sansjson/tests/test_sort_list.py
+++ sansjson/tests/test_sort_list.py
@@ -10,3 +10,7 @@ def test_sortable_homogenous():
 def test_sortable_nonhomogenous():
     sJ = [None, 'a', 2, None]
     assert sansjson.sort(sJ) == [None, None, 2, 'a']
+
+    o = object()
+    sJ = [None, 'z', 2, None, o, True, False, 1, 'a']
+    assert sansjson.sort(sJ) == [None, None, False, True, 1, 2, 'a', 'z', o]
