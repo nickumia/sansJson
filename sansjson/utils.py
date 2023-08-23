b4d65be78a97e1d240bc51a45b4363f60ba4e1f2
nickumia <nickumia6@gmail.com>
2023-08-22 22:53:26 -0400

    New Feature: Sort `list` of `dict` (#4)
    * new: wip list of dict sorting
    
    - minor support for list of dicts
    - add function to compare dicts (recursively check key-value pairs for 'smaller' component)
    - add new BRANCH for list of dicts in sorting function
    
    * new: list of dicts fully functional
    
    Lots to document, but at least save the state :')
    
    * refactor: simplify Sorter.sort
    
    - LEAF/BRANCH structure breaks down, but lists are essentially 'seeds' that can branch further if they fit the type to be nourished by being a dict
    - SPROUT speaks to the above difference
    - condensed code might be harder to read, but that's what the comments are for
    
    * docs/refactor: the most complicated function :(
    
    - Function to compare dictionaries is overwhelming... I want to optimize it, but I'm not entirely sure how to yet..
    - It solves the job of sorting dictionaries by defining the method to sort.. however, it is a bit overloaded since lists are allowed as values in dictionaries... since lists can be values and lists can have dictionaries in them, it created a double recursive problem...
    - nonhomogenous(d) (list) --> sorted(d, key=functools.cmp_to_key(dict_sort_key)) (dict) --> dict_sort_key (list as value) --> dict_sort_key (dictionary as value) --> dict_sort_key (output smallest key between shared dicts) --> dict_sort_key (first unique key between both dicts)
    - I'm not proud of that function, it's still messy :(
    - NOTE: for the case when dictionary value is a list, PYTHON '>/<' operators use the 'max' value of the list to determine greater/lesser than, there is not defined 'max' for a list of dicts, so the 'dict_sort_key' sorts the keys for both dictionaries and returns the sorted list which has the smallest key first and is then used to find the dictionary with the smallest unique key-value pair
    
    * new: bump version
    
    this was a pretty big feature add
    
    * lint: blah
    
    * new: test cases
    
    Minor variations that hits all lines of the complex dict_sort_keys function
    
    * fix: don't do default
    
    I thought this was a good default to have.. but it broke one of the new tests.. so taking it out.  This was previously untested
    
    * refactor: k1 --> k_a and k2 --> k_b
    
    k1 should denote the first key in any dictionary, k_a is a given key in dict a and k_b is the a given key in dict_b
    
    ---------
    
    Co-authored-by: Nicholas Kumia <nicholas.kumia@reisystems.com>

--- sansjson/utils.py
+++ sansjson/utils.py
@@ -1,10 +1,15 @@
 
+from copy import deepcopy
+import functools
 import json
 import logging
 
 log = logging.getLogger(__name__)
 
-NONHOMOGENOUS_ORDER = [type(None), bool, int, float, str, list, object]
+NONHOMOGENOUS_ORDER = {
+    type(None): 1, bool: 2, int: 3, float: 4, str: 5, list: 6,
+    object: 7, dict: 8
+}
 
 
 class Hasher:
@@ -67,26 +72,145 @@ class Sorter(Hasher):
             return True
         return False
 
+    def recursive_dict(self, context):
+        sorted_dict = {}
+        keys = nonhomogenous(context.keys())
+        for k in keys:
+            if self.is_sortable(context[k]):
+                # BRANCH: recursively sort values
+                sorted_dict[k] = self.sort(context[k])
+            else:
+                # LEAF: basic data types
+                sorted_dict[k] = context[k]
+        return sorted_dict
+
     def sort(self, context=None):
         sorted_dict = {}
         if context is None:
             context = self.data
 
         if isinstance(context, list):
-            # LEAF: just sort list
-            return nonhomogenous(context)
+            # LEAF/SPROUT: list of anything
+            # LEAF component -> it's just a list
+            # SPROUT component -> dicts need further attention
+            return nonhomogenous([
+                self.recursive_dict(element) if isinstance(element, dict)
+                else element for element in context
+            ])
         if isinstance(context, dict):
-            # BRANCH: sort keys
-            keys = nonhomogenous(context.keys())
-            for k in keys:
-                if self.is_sortable(context[k]):
-                    # BRANCH: recursively check values
-                    sorted_dict[k] = self.sort(context[k])
-                else:
-                    # LEAF: basic data types
-                    sorted_dict[k] = context[k]
+            # BRANCH: sort keys + values
+            sorted_dict = self.recursive_dict(context)
 
         return sorted_dict
+
+
+def dict_sort_key(dicta, dictb):
+    '''
+    Custom dictionary comparison function
+
+    Iterate through all key-value pairs.  The first key or value that is
+    smaller decides the sorting order.
+    e.g.1.  a = {'q': 8, 'w': 2}
+            b = {'q': 2, 'w': 8}
+        --> b is smaller because 2 < 8 for 'q' key.
+
+    e.g.2.  a = {'q': 2, 'w': 3}
+            b = {'q': 2, 'w': 0}
+        --> b is smaller because 0 < 3 for 'w' key.
+
+    e.g.3.  a = {'q': 0, 'w': 0}
+            b = {'q': 0, 'w': 0}
+        --> a is smaller because both are identical and a was passed first.
+
+    -1 --> dicta is smaller
+    +1 --> dictb is smaller
+    0 --> dicta == dictb
+
+    NOTE 1: JSON keys are guaranteed to be 'str'.  This function has that
+    assumption embedded.
+    NOTE 2: Dictionaries are assumed to be sorted before this function call.
+    '''
+    dicta_list = False
+    dictb_list = False
+    try:
+        k_a = list(dicta.keys())[0]
+    except IndexError:
+        # No more keys
+        k_a = None
+    except AttributeError:
+        # list to list compare
+        dicta_list = sorted([dicta], key=functools.cmp_to_key(dict_sort_key))
+    try:
+        k_b = list(dictb.keys())[0]
+    except IndexError:
+        # No more keys
+        k_b = None
+    except AttributeError:
+        # list to list compare
+        dictb_list = sorted([dictb], key=functools.cmp_to_key(dict_sort_key))
+
+    if dicta_list and dictb_list:
+        # WORST NIGHTMARE: full sorting not possible
+        # Find the smallest keys between both dicts
+        return sorted(dicta_list[0] + dictb_list[0],
+                      key=functools.cmp_to_key(dict_sort_key))
+
+    # Dict with less keys is 'smaller'
+    if k_a is None and k_b is None:
+        return 0
+    if k_a is None and k_b is not None:
+        return -1
+    if k_a is not None and k_b is None:
+        return 1
+
+    # First, compare keys
+    if k_a == k_b:
+        v1_type = type(dicta[k_a])
+        v2_type = type(dictb[k_b])
+        # Second, compare values
+        if v1_type != v2_type:
+            # Most likely scenario -> one value is a list and the other is not.
+            # Use NONHOMOGENOUS_ORDER to determine sorting priority
+            if NONHOMOGENOUS_ORDER[v1_type] > NONHOMOGENOUS_ORDER[v2_type]:
+                return 1
+            else:
+                return -1
+        else:
+            if dicta[k_a] == dictb[k_b]:
+                # key-value pairs are equal ...
+                # Delete them and move to the next key-value
+                dicta_copy = deepcopy(dicta)
+                dictb_copy = deepcopy(dictb)
+                del dicta_copy[k_a], dictb_copy[k_b]
+                return dict_sort_key(dicta_copy, dictb_copy)
+            else:
+                # WORST NIGHTMARE: same as the last nightmare ;)
+                if isinstance(dicta[k_a], list) and isinstance(dictb[k_b], list):  # NOQA E501
+                    if isinstance(dicta[k_a][0], dict) and isinstance(dictb[k_b][0], dict):  # NOQA E501
+                        smallest_key = dict_sort_key(dicta[k_a], dictb[k_b])
+                        # Find the first unique key that is smaller.
+                        # Otherwise, all keys are the same, default to
+                        # first dict as smaller.
+                        for dict_items in smallest_key:
+                            for key, value in dict_items.items():
+                                if key in dicta[k_a][0]:
+                                    if dicta[k_a][0][key] == value:
+                                        if key not in dictb[k_b][0]:
+                                            return -1
+                                elif key in dictb[k_b][0]:
+                                    if dictb[k_b][0][key] == value:
+                                        if key not in dicta[k_a][0]:
+                                            return 1
+                elif dicta[k_a] > dictb[k_b]:
+                    return 1
+                elif dicta[k_a] < dictb[k_b]:
+                    return -1
+                return 0
+    else:
+        if k_a > k_b:
+            return 1
+        else:
+            return -1
 
 
 def nonhomogenous(sans):
@@ -113,6 +237,9 @@ def nonhomogenous(sans):
             if dt in data_groups.keys():
                 if isinstance(None, dt):
                     final += data_groups[dt]
+                elif dict == dt:
+                    final += sorted(data_groups[dt],
+                                    key=functools.cmp_to_key(dict_sort_key))
                 else:
                     final += sorted(data_groups[dt])
 
